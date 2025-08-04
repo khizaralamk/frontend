46800c6e3d0584e63f79c43d40361b79514385a2
khizaralamk <khizar@alphaventure.com>
2025-08-04 23:16:07 +0500

    v0.0.5

--- src/components/OurProducts.tsx
+++ src/components/OurProducts.tsx
@@ -4,19 +4,19 @@ const productCategories = [
   {
     id: 1,
     name: "Jerseys",
-    image: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    image: "https://wallpapercave.com/wp/wp10209855.jpg",
     description: "Official team jerseys"
   },
   {
     id: 2,
     name: "Trousers",
-    image: "https://images.unsplash.com/photo-1544966503-7cc5ac882d5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    image: "https://images.unsplash.com/photo-1560272564-c83b66b1ad12?ixlib=rb-1.2.1&w=1000&q=80",
     description: "Sports trousers & shorts"
   },
   {
     id: 3,
     name: "Sports Items",
-    image: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    image: "https://wallpaperaccess.com/full/1594708.jpg",
     description: "Accessories & equipment"
   }
 ]
